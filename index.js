55fa4810598d4ff000a846cb87954a7f95421bb3
diaztawa <diaztawa3@gmail.com>
2020-03-05 13:03:13 +0800

    Hasil Video ke-2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const electron = require("electron");
 
-const{app, BrowserWindow, MEnu, ipcMain} = electron;
+const{app, BrowserWindow, Menu, ipcMain} = electron;
 
 let todayWindow;
 let creteWindow;
@@ -21,4 +21,69 @@ app.on("ready", ()=> {
         app.quit();
         todayWindow = null;
     });
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+
 });
+
+const listWindowCreator = () => {
+    listWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 600,
+        height: 400,
+        title: "All Appointments"
+    });
+
+    listWindow.setMenu(null);
+    listWindow.loadURL(`file://${__dirname}/list.html`);
+    listWindow.on("closed", () => (listWindow = null));
+};
+
+const createWindowCreator = () => {
+    createWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        },
+        width: 600,
+        height: 400,
+        title: "Create Appointments"
+    });
+
+    createWindow.setMenu(null);
+    createWindow.loadURL(`file://${__dirname}/create.html`);
+    createWindow.on("closed", () => (createWindow = null));
+};
+
+const menuTemplate = [{
+    label: "File",
+    submenu: [{
+        label: "New Appointment",
+
+        click(){
+            createWindowCreator();
+        }
+    },
+    {
+        label: "All Appointments",
+        click(){
+            listWindowCreator();
+        }
+    },
+    {
+        label: "Quit",
+        click(){
+            app.quit();
+        }
+    }
+]
+},
+
+{
+    label: "View",
+    submenu: [{role: "reload"}, {role: "toggledevtools"}]
+}
+
+]
